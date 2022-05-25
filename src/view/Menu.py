b5966de8e42c398bf8548323cd57bbde14924d5f
Luca Morgado <luca.morgado@ynov.com>
2022-05-25 20:53:46 +0200

    fix help screen

--- src/view/Menu.py
+++ src/view/Menu.py
@@ -7,23 +7,27 @@ from src.control.eventKey import EventKey
 
 
 class MenuButton(Button):
-    def __init__(self, func,**kwargs):
+    def __init__(self, func, **kwargs):
         super().__init__(**kwargs)
         self.func = func
-    
+
     def on_release(self):
         self.func()
         return super().on_release()
 
-class MenuScreen(Screen,EventKey):
-    def __init__(self,**kwargs) -> None:
-        Screen.__init__(self,**kwargs)
+
+class MenuScreen(Screen, EventKey):
+    def __init__(self, **kwargs) -> None:
+        Screen.__init__(self, **kwargs)
         EventKey.__init__(self)
-        self.options = {"Load Game":lambda  : print("Load Game"),"New Game":lambda : self.manager.Switch("newgame"),"Help":lambda :print("help"),"Quit":lambda : exit(0)}
+        self.options = {
+            "Load Game": lambda: print("Load Game"),
+            "New Game": lambda: self.manager.Switch("newgame"),
+            "Help": lambda: self.manager.Switch("help"),
+            "Quit": lambda: exit(0),
+        }
         t = []
         for key in self.options:
             t.append(key)
-            button = MenuButton(self.options[key],text=str(len(t))+". "+key)
+            button = MenuButton(self.options[key], text=str(len(t)) + ". " + key)
             self.ids.gridMenu.add_widget(button)
-
-        
