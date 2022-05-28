9bdfda1326371e5ce8018050580939ee44df57ea
mkarten <morgadoluca03@gmail.com>
2022-05-28 10:54:05 +0200

    open issue button

--- src/view/Help.py
+++ src/view/Help.py
@@ -1,12 +1,16 @@
 
 from kivy.uix.screenmanager import  Screen
 from src.control.eventKey import EventKey
+import webbrowser
 
 class  HelpScreen(Screen):
         def __init__(self,**kwargs):
             Screen.__init__(self,**kwargs)
             EventKey.__init__(self)
             
+        def OpenIssuePage(self):
+            webbrowser.open("https://github.com/tot0p/RPG/issues")
+
         def key_action(self, keybord, keycode, _, keyName, textContent):
             if keycode == 27:
                 self.manager.Switch("menu")
